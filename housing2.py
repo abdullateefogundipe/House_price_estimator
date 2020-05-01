98c1b9e6c7efe71b3d525d315a0cdc7f492fe8cc
abdullateefogundipe <51425002+abdullateefogundipe@users.noreply.github.com>
2020-05-01 10:47:15 +0100

    Add files via upload

--- housing2.py
+++ housing2.py
@@ -3,6 +3,9 @@ import joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
 import matplotlib
 matplotlib.use('agg')
 
